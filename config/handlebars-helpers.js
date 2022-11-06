c8c80e4e886522ae17c269da32caae4b3b12153b
OverFitted <kozak.bor@gmail.com>
2022-11-06 22:26:01 +0300

    refactor: remove duplicate helper-function

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -66,56 +66,6 @@ module.exports = {
         }
 
     },
-    comp: function (lvalue, rvalue, options) {
-        if (arguments.length < 3)
-            throw new Error("Handlebars Helper 'compare' needs 2 parameters");
-        if (typeof rvalue !== "string") {
-            rvalue = lvalue;
-        }
-
-        let operator = options.hash.operator || "==";
-
-        let operators = {
-            '==': function (l, r) {
-                return l == r;
-            },
-            '===': function (l, r) {
-                return l === r;
-            },
-            '!=': function (l, r) {
-                return l != r;
-            },
-            '<': function (l, r) {
-                return l < r;
-            },
-            '>': function (l, r) {
-                return l > r;
-            },
-            '<=': function (l, r) {
-                return l <= r;
-            },
-            '>=': function (l, r) {
-                return l >= r;
-            },
-            'typeof': function (l, r) {
-                return typeof l == r;
-            },
-            '%': function (l, r) {
-                return l % r === 0;
-            }
-        }
-
-        if (!operators[operator])
-            throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
-
-        let result = operators[operator](lvalue, rvalue);
-
-        if (result) {
-            return options.fn(this);
-        } else {
-            return options.inverse(this);
-        }
-    },
     getFromArr: function (array, index) {
         return array[index]
     },
